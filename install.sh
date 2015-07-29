b7a7c76b069636504207358508a8bf6563b64951
Stephen Olander-Waters <stephenw@stedwards.edu>
2015-07-29 09:10:55 -0500

    Add example configuration and fully qualified paths to install.sh

--- install.sh
+++ install.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-groupadd -f printtobox
+/usr/sbin/groupadd -f printtobox
 
-mkdir -m '2770' /var/cache/PrintToBox
-chgrp printtobox /var/cache/PrintToBox
+/bin/mkdir -m '2770' /var/cache/PrintToBox
+/bin/chgrp printtobox /var/cache/PrintToBox
 
-cp PrintToBox.conf /etc
-chmod 0640 /etc/PrintToBox.conf
+/bin/cp PrintToBox.conf /etc
+/bin/chmod 0640 /etc/PrintToBox.conf
 
-mkdir -m '0755' /usr/lib/PrintToBox
+/bin/mkdir -m '0755' /usr/lib/PrintToBox
 
-cp build/libs/PrintToBox-1.0.jar /usr/lib/PrintToBox/
-chmod 0644 /usr/lib/PrintToBox/PrintToBox-1.0.jar
+/bin/cp build/libs/PrintToBox-1.0.jar /usr/lib/PrintToBox/
+/bin/chmod 0644 /usr/lib/PrintToBox/PrintToBox-1.0.jar
 
-cp PrintToBox.sh /usr/lib/PrintToBox/
-chmod 0755 /usr/lib/PrintToBox/PrintToBox.sh
+/bin/cp PrintToBox.sh /usr/lib/PrintToBox/
+/bin/chmod 0755 /usr/lib/PrintToBox/PrintToBox.sh
 
-ln -s /usr/lib/PrintToBox/PrintToBox.sh /usr/bin/PrintToBox
+/bin/ln -s /usr/lib/PrintToBox/PrintToBox.sh /usr/bin/PrintToBox
